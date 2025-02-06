f370301c9ebeb797ca8ffbc5f6ab4ac52b1fe635
Fardin <faa35@sfu.ca>
2025-02-05 17:57:18 -0800

    commented out test 11 classes and methods

--- src/test/java/com/genie/journey_genie/models/ChecklistTest.java
+++ src/test/java/com/genie/journey_genie/models/ChecklistTest.java
@@ -1,32 +1,32 @@
-// Java file for testing the Checklist class
+// // Java file for testing the Checklist class
 
-package com.genie.journey_genie.models;
+// package com.genie.journey_genie.models;
 
-import java.util.List;
-import java.util.ArrayList;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// import java.util.List;
+// import java.util.ArrayList;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
-public class ChecklistTest {
-    @Test
-    void checklistTest() {
-        // Creating the checklist
-        Checklist checklist = new Checklist();
+// public class ChecklistTest {
+//     @Test
+//     void checklistTest() {
+//         // Creating the checklist
+//         Checklist checklist = new Checklist();
 
-        // Creating lists
-        List<String> activities = new ArrayList<>();
-        activities.add("Hiking");
-        List<String> places = new ArrayList<>();
-        places.add("Vancouver");
+//         // Creating lists
+//         List<String> activities = new ArrayList<>();
+//         activities.add("Hiking");
+//         List<String> places = new ArrayList<>();
+//         places.add("Vancouver");
 
 
-        // Testing its methods
+//         // Testing its methods
 
-        checklist.setActivities(activities);
-        assertEquals(activities, checklist.getActivities());
+//         checklist.setActivities(activities);
+//         assertEquals(activities, checklist.getActivities());
         
-        checklist.setPlaces(places);
-        assertEquals(places, checklist.getPlaces());
-    }   
+//         checklist.setPlaces(places);
+//         assertEquals(places, checklist.getPlaces());
+//     }   
 
-}
+// }
